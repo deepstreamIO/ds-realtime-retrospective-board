0e1faf5d45ee8837010f379663d8a0eb89393fd1
Yael <yaelst@gmail.com>
2017-03-03 15:05:10 +0100

    improved

--- client/js/app.js
+++ client/js/app.js
@@ -1,22 +1,21 @@
 $(function(){
-	var ds = deepstream( 'localhost:6020' );
+	var ds = deepstream('APP URL');
 
 	$( 'form' ).on( 'submit', function( event ){
 		event.preventDefault();
 
 		var authData = {
-			username: $( 'form input[type="text"]' ).val(),
+			type: 'email',
+			email: $( 'form input[type="text"]' ).val(),
 			password: $( 'form input[type="password"]' ).val()
 		};
-	
 		ds.login( authData, function( success, errorEvent, errorMessage ) {
 			if( success ) {
 				new StickyNoteBoard( ds );
-				new VideoChat( ds, authData.username );
 				$( '.login' ).hide();
 			} else {
 				$( '.login-error' ).text( errorMessage ).show();
-			}		
+			}
 		});
 	});
 });
